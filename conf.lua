d03c904f3d0b1bc8dd3a4bedc86160aff164f1de
Muna Alaneme <munachisoalaneme676@gmail.com>
2024-10-23 14:25:35 -0400

    1

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+	t.author = "MunaAlaneme" --MunaAlaneme made this game with a little help. :)
+	t.identity = "LuaLovePlatformer"
+	--t.console = true
+	--t.screen = false
+	t.modules.physics = false
+	t.version = "11.5"
+	t.window = true
+	t.externalstorage = true
+end
